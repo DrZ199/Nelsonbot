0d85fab034952a1cdac837a39fddca4bbdf9cd21
DrZ199 <190632761+DrZ199@users.noreply.github.com>
2024-12-11 07:10:10 +0500

    footer.jsx

---
+++ src/components/footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import '../styles/footer.css';
+
+function Footer() {
+  return (
+    <footer className="footer">
+      <p>&copy; 2023 NelsonBot. All rights reserved.</p>
+      <p>Powered by OpenAI API</p>
+    </footer>
+  );
+}
+
+export default Footer;
+
